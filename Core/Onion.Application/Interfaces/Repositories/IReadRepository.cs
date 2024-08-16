29f5187b220c1a48b22884ce984f345bb79ae56c
Nurullah Ahmet Arıkan <nurullahmetarikan@gmail.com>
2024-08-16 22:45:19 +0300

    Repository'ler yazıldı.

---
+++ Core/Onion.Application/Interfaces/Repositories/IReadRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+using Onion.Domain.Common;
+
+namespace Onion.Application.Interfaces.Repositories
+{
+    public interface IReadRepository<T> where T : class,IEntityBase,new() 
+    {
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
+          Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+          Func<IQueryable<T>, IOrderedQueryable<T>>? orederedBy = null,
+          bool enableTracking = false);//-->Read yaparken tracking'in çalışmaması için
+
+
+        Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null,
+           Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+           Func<IQueryable<T>, IOrderedQueryable<T>>? orederedBy = null,
+           bool enableTracking = false, int currentPage = 1, int pageSize = 3);//-->Read yaparken tracking'in çalışmaması için
+
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include,
+            bool enableTracking = false);//-->Read yaparken tracking'in çalışmaması için
+
+
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate,bool enableTracing=false);
+
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+
+    }
+}
--- Infrastructure/Onion.Persistence/Context/Registeration.cs
+++ Infrastructure/Onion.Persistence/Context/Registeration.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Onion.Application.Interfaces.Repositories;
+using Onion.Persistence.Repositories;
 
 namespace Onion.Persistence.Context
 {
@@ -14,6 +16,9 @@ namespace Onion.Persistence.Context
         public static void AddPersistence(this IServiceCollection services, IConfiguration _configuration) 
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof (IReadRepository<>), typeof (ReadRepository<>));
+            services.AddScoped(typeof (IWriteRepository<>), typeof (WriteRepository<>));
         }
        
     }
