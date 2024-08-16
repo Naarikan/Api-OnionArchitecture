9cabf36386f4713cc372e4f393ebcbf1e8309b54
Nurullah Ahmet Arıkan <nurullahmetarikan@gmail.com>
2024-08-16 19:23:05 +0300

    Migration işlemi tamamlandı.Ayrıca bazı sınıflar için boş configuration ayaralrı yapıldı fakat bunun sonucunda hata alındığı için sadece Brand için olan bırakıldı.

---
+++ Infrastructure/Onion.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onion.Domain.Entities;
+
+namespace Onion.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        //Diğer configurationdan bazılarının içini doldurmadığım için hata verdiği için silinmiştir.
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x=>x.Name).HasMaxLength(256);
+        }
+    }
+}
---
+++ Infrastructure/Onion.Persistence/Context/Registeration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Onion.Persistence.Context
+{
+    public static class Registeration
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration _configuration) 
+        {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+        }
+       
+    }
+}
